0cf9bd4fde6d986a15c9829f443063f4b5bca8dd
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-08-11 23:06:37 +0530

    Initial setup configured

---
+++ flappy-frontend/app/(tabs)/leaderboard.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function leaderboard() {
+  return (
+    <View>
+      <Text>leaderboard</Text>
+    </View>
+  )
+}
