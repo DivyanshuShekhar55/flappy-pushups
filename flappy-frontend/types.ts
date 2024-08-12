86492478e65de12d5c6c2c5c75b5aabd4f87049f
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-08-12 13:49:14 +0530

    added types.ts file

---
+++ flappy-frontend/types.ts
@@ -0,0 +1,22 @@
+export interface Bird {
+  y_pos: number;
+  x_pos: number;
+}
+
+export interface Pipe {
+  x_pox: number;
+  y_pos: number;
+  vx: number;
+  height: number;
+  width: number;
+}
+
+export interface SharedValue<Value = unknown> {
+  value: Value;
+  addListener: (listenerID: number, listener: (value: Value) => void) => void;
+  removeListener: (listenerID: number) => void;
+  modify: (
+    modifier?: <T extends Value>(value: T) => T,
+    forceUpdate?: boolean
+  ) => void;
+}
