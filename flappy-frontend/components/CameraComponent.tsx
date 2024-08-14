c67b2a7bf2c3d8c73b31538cacb478d062a09ebc
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-08-14 22:23:51 +0530

    removed conflicting error from camera file name

---
+++ flappy-frontend/components/CameraComponent.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CameraComponent() {
+  return (
+    <View>
+      <Text>CameraComponent</Text>
+    </View>
+  )
+}
