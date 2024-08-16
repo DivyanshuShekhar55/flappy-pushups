eae947242fff24b61d39ddf7c379882e21a2f81a
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-08-16 13:39:10 +0530

    minorr changes to leaderboard so no 2 tabs open camera

--- flappy-frontend/app/(tabs)/leaderboard.tsx
+++ flappy-frontend/app/(tabs)/leaderboard.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CameraComponent from '@/components/CameraComponent'
+import Score from '@/components/Score'
 
 export default function leaderboard() {
   return (
-    <View>
-      <Text>Leaderboard</Text>
+    <View style={{backgroundColor:'white', flex:1, marginTop:100}}>
+      <Score/>
     </View>
   )
 }
